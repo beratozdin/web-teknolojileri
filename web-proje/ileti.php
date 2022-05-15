7c1c407acc84e8aa99db1e3643048dc56c905369
Berat Özdin <32714982+beratozdin@users.noreply.github.com>
2022-05-15 14:45:48 +0300

    Add files via upload

--- web-proje/ileti.php
+++ web-proje/ileti.php
@@ -25,70 +25,67 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-success">
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-success">
 
-    <div class="container">
+  <div class="container">
 
-        <a class="navbar-brand" href="sehrim.html"><i class="fas fa-person-walking-luggage"></i>Tekirdağ</a>
+    <a class="navbar-brand" href="sehrim.html"><i class="fas fa-person-walking-luggage"></i>Tekirdağ</a>
 
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.html">Hakkında</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="ozgecmis.html">Özgeçmiş</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="sehrim.html">Şehrim</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="ilgialani.html">İlgi Alanlarım</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="mirasimiz.html">Mirasımız</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="login.html">Giriş Yap</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="ilet.html">İletişim</a>
-            </li>     
-        </ul>
-        </div>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">Hakkında</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="ozgecmis.html">Özgeçmiş</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="sehrim.html">Şehrim</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="ilgialani.html">İlgi Alanlarım</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="mirasimiz.html">Mirasımız</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="login.html">Giriş Yap</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="iletisim.html">İletişim</a>
+        </li>     
+      </ul>
     </div>
-    </nav>
-    <br><br><br><br>
-    <div class="section">
-    <h1 class="text-center">API Servisi</h1>
-    </div>
-    <header>
-        <div class="container">
-            <div class="row"> 
-                <div class="col" ></div>
-                <div class="col-sm-10">
-                    <br><br>  
-                    
-                    <div id="rs" class="container">
-                        <h2>Veri</h2>
-                    </div>
-            </div>
-        </div>
+  </div>
+</nav>
+<br><br>
+    <header>       
+        <br><br><br>
+        <h1>İletişim Formundan Gelen Bilgiler</h1>
+       
     </header>
+    <main class="justify">
 
-    <main>
+        <div class="container mt-5 w-50 text-center p-5">
+        
 
-        <br><br><br><br><br>
-        <div class="container" >
-
+            <?php     
+                echo($_POST["name"]); echo ("\n"); 
+                echo($_POST["surname"]); echo ("\n");  
+                echo($_POST["email"]); echo ("\n");  
+                echo($_POST["baslik"]); echo ("\n");  
+                echo($_POST["konu"]); echo ("\n");  
+         
+            ?>      
         </div>
     </main>
+
 <!-- Footer -->
 <footer class="bg-danger text-center text-light">
     
@@ -116,7 +113,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<script src="./js/main.js"></script>
+
 </body>
 
 </html>
